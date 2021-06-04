b6e748969717b6ca0e281ce6a5e8cc5aa51d3713
faranix <stephane59119@gmail.com>
2021-06-04 14:25:41 +0200

    Porjet finis

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
 // Import de scss
-import '@/assets/sass/App.scss'
+import '@/assets/sass/App.scss';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
